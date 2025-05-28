67e2befc1c2f9acdb179552011c7d53db3154f9a
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2025-05-28 18:33:34 +0300

    header add burger menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.querySelector('.burger');
+  const menu = document.getElementById('mobileMenu');
+
+  burger.addEventListener('click', function () {
+    menu.classList.toggle('show');
+
+    // Міняємо символ кнопки
+    if (menu.classList.contains('show')) {
+      burger.innerHTML = '←';
+      burger.setAttribute('aria-label', 'Закрити меню');
+    } else {
+      burger.innerHTML = '☰';
+      burger.setAttribute('aria-label', 'Відкрити меню');
+    }
+  });
+});
+
